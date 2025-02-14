cfe01c4e2a8e9b8ae2e7d27e87ef2ce14c99e48e
Nimsara200 <Gamage@2024>
2025-02-14 18:26:19 +0530

    leaves check 2

--- frontend/src/components/leaveManage.js
+++ frontend/src/components/leaveManage.js
@@ -15,7 +15,7 @@ function LeavesTable() {
   const fetchLeaves = async () => {
     try {
       const response = await axios.get(
-        "https://gamage-rebuild-p9ce9adut-sunera9s-projects.vercel.app/leaves/get"
+        "https://gamage-rebuild.vercel.app/leaves/get"
       );
       setLeaves(response.data || []); // Ensure leaves is an array
     } catch (error) {
