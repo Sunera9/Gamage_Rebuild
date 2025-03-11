5602d00d7c5c6e1f4e71a71cf85682d0ea3c4e97
Nimsara200 <Gamage@2024>
2025-03-11 11:29:43 +0530

    test

--- backend/models/applicationForm.js
+++ backend/models/applicationForm.js
@@ -1,49 +1,48 @@
 const mongoose = require('mongoose');
 
 
-const ApplicationFormSchema = new mongoose.Schema(
-{
-  userName: { 
-    type: String, 
-    required: false 
-},
-  userNIC: { 
-    type: String, 
-    required: false 
-},
-  userEmail: { 
-    type: String, 
-    required: false 
-},
-  contactNumber: { 
-    type: String, 
-    required: false 
-},
+const ApplicationFormSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  userNIC: {
+    type: String,
+    required: false,
+  },
+  userEmail: {
+    type: String,
+    required: false,
+  },
+  contactNumber: {
+    type: String,
+    required: false,
+  },
   userAddress: {
-    type: String, 
-    required: false 
-},
-  jobId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Job', 
-    required: false 
-},
-  jobName: { 
-    type: String, 
-    required: false 
-},
-  coverLetter: { 
-    type: String 
-},
-  resume: { 
-    type: String 
-},
-  createdAt: { 
-    type: Date, 
-    default: Date.now 
-},
-}
-);
+    type: String,
+    required: false,
+  },
+  jobId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job",
+    required: false,
+  },
+  jobName: {
+    type: String,
+    required: false,
+  },
+  coverLetter: {
+    type: String,
+  },
+  resume: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model('ApplicationForm', ApplicationFormSchema);
 
